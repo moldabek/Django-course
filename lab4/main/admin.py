2c6be40350f26f348a8fffa78766115eb088c8b8
Beibarys Moldabekov <bejbarys.moldabekov@bk.ru>
2021-03-05 15:00:54 +0600

    fix(admin.py):filter and search added

--- lab4/main/admin.py
+++ lab4/main/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-
 from main.models import List, Task
 
-admin.site.register(List)
-admin.site.register(Task)
+
+class TaskInline(admin.TabularInline):
+    model = Task
+
+
+class ListAdmin(admin.ModelAdmin):
+    inlines = [
+        TaskInline,
+    ]
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ['created', 'due_on', 'mark']
+    search_fields = ['name', 'owner']
+
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Task, TaskAdmin)
